a09f6a16ab6ea0f578e2eb814d20bd664ddd7fe9
Madhackr <115146912+Madhackr@users.noreply.github.com>
2022-10-10 12:05:04 +0600

    Add files via upload

---
+++ mad001.py
@@ -0,0 +1,43 @@
+
+import os
+import time
+import sys
+
+os.system("clear")
+
+os.system("apt install cmatrix")
+os.system("apt install libcaca")
+
+
+os.system("clear")
+
+
+
+z="""
+
+              ███╗░░░███╗░█████╗░██████╗░
+              ████╗░████║██╔══██╗██╔══██╗
+              ██╔████╔██║███████║██║░░██║
+              ██║╚██╔╝██║██╔══██║██║░░██║
+              ██║░╚═╝░██║██║░░██║██████╔╝
+              ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═════╝░
+   ╔═══════════════════════════════════════════════╗
+   ║       [✓] TOOL NAME : MAD-X                            ║ 
+   ║       [✓] YOUTUBE : MAD-X                              ║
+   ║       [✓] GITHUB : MAD-x                               ║
+   ║       [✓] EMAIL :madhackr001@gmail.com                 ║
+   ╚═══════════════════════════════════════════════╝
+  
+"""
+
+print(z)
+
+print("cmatrix")
+
+print("　")
+ 
+print("libcaca")  
+print("　")
+print(str("Enter tools name :"))
+
+
