e45b16404ddd456b2f634cc115bf8f1a8aa28672
Madhackr <115146912+Madhackr@users.noreply.github.com>
2022-10-10 11:47:58 +0600

    Add files via upload

---
+++ mad-fane.py
@@ -0,0 +1,45 @@
+
+import os
+import time
+import sys
+
+os.system("clear")
+
+os.system("apt install cmatrix")
+os.system("apt install libcaca")
+
+
+os.system("clear")
+
+
+
+z="""
+
+              ███╗░░░███╗░█████╗░██████╗░
+              ████╗░████║██╔══██╗██╔══██╗
+              ██╔████╔██║███████║██║░░██║
+              ██║╚██╔╝██║██╔══██║██║░░██║
+              ██║░╚═╝░██║██║░░██║██████╔╝
+              ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═════╝░
+   ╔═══════════════════════════════════════════════╗
+   ║       [✓] TOOL NAME : MAD-X                            ║ 
+   ║       [✓] YOUTUBE : MAD-X                              ║
+   ║       [✓] GITHUB : MAD-x                               ║
+   ║       [✓] EMAIL :madhackr001@gmail.com                 ║
+   ╚═══════════════════════════════════════════════╝
+  
+"""
+
+print(z)
+cmatrix="""
+os.system("cmatrix")
+"""
+print("cmatrix")
+   
+os.system("　")   
+ print("cacafire")  
+ 
+ os.system("　")   
+print(str("Enter tools name  "))
+
+
