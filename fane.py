678df3e88a254c17ce06974b4c76abddd9929e00
Madhackr <115146912+Madhackr@users.noreply.github.com>
2022-10-10 11:09:07 +0600

    Add files via upload

---
+++ fane.py
@@ -0,0 +1,38 @@
+
+import os
+import time
+import sys
+
+os.system("clear")
+
+os.system("apt install cmatrix")
+
+
+os.system("clear")
+
+
+
+z="""
+
+              ███╗░░░███╗░█████╗░██████╗░
+              ████╗░████║██╔══██╗██╔══██╗
+              ██╔████╔██║███████║██║░░██║
+              ██║╚██╔╝██║██╔══██║██║░░██║
+              ██║░╚═╝░██║██║░░██║██████╔╝
+              ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═════╝░
+   ╔═══════════════════════════════════════════════╗
+   ║       [✓] TOOL NAME : MAD-X                            ║ 
+   ║       [✓] YOUTUBE : MAD-X                              ║
+   ║       [✓] GITHUB : MAD-x                               ║
+   ║       [✓] EMAIL :madhackr001@gmail.com                 ║
+   ╚═══════════════════════════════════════════════╝
+  
+"""
+
+print(z)
+a="""
+os.system("cmatrix")
+"""
+print("[a] cmatrix")
+
+str(input("Enter tools namber: "))
